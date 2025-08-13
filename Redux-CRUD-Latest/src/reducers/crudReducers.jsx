ad1cfe54b771b5d2489f13349d6c5b3636205c64
DEEPAK-SINGH <149694925+DEEPAK-SINGH-GOOR@users.noreply.github.com>
2025-08-13 07:23:18 +0530

    Update crudReducers.jsx

--- Redux-CRUD-Latest/src/reducers/crudReducers.jsx
+++ Redux-CRUD-Latest/src/reducers/crudReducers.jsx
@@ -3,20 +3,31 @@ const initialState = {
 };
 
 export default function todoReducer(state = initialState, action) {
+  console.log("Reducer received action:", action.type, "with payload:", action.payload);
+
   switch (action.type) {
     case "ADD_TODO":
+      console.log("Before ADD:", state.todos);
+      console.log("After ADD:", [...state.todos, action.payload]);
       return {
         ...state,
         todos: [...state.todos, action.payload],
       };
 
     case "REMOVE_TODO":
+      console.log("Before REMOVE:", state.todos);
+      console.log("After REMOVE:", state.todos.filter((todo) => todo.id !== action.payload));
       return {
         ...state,
         todos: state.todos.filter((todo) => todo.id !== action.payload),
       };
 
     case "UPDATE_TODO":
+      console.log("Before UPDATE:", state.todos);
+      console.log(
+        "After UPDATE:",
+        state.todos.map((t) => (t.id === action.payload.id ? action.payload : t))
+      );
       return {
         ...state,
         todos: state.todos.map((t) =>
